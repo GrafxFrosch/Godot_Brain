b2b7c90a0a90bb7c6a34e6c0c340d6a301ad86e1
pascal.huppertz <huppertz@se-rwth.de>
2026-02-04 10:09:55 +0100

    initial

---
+++ godot-brain/addons/brain/brain.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@export var eyes: Camera2D
+@export var body: CharacterBody2D
+@export var task: String
+@export var API_KEY: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
